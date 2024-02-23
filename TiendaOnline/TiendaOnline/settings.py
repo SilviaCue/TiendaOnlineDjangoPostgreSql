481d27c85cf3bd12daec9655710310343681dd16
SilviaCue <152323429+SilviaCue@users.noreply.github.com>
2024-02-23 16:41:27 +0100

    tablas PostgreSQL

--- TiendaOnline/TiendaOnline/settings.py
+++ TiendaOnline/TiendaOnline/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'GestionPedidos',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'articulosclientes',
+        'USER': 'postgres',
+        'PASSWORD':'Anando.26',
+        'HOST': '127.0.0.1',  
+        'PORT':'5432',        
     }
 }
 
