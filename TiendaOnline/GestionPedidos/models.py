00071a9cfa7c2fcc92ba19911d2915f4f88f87ee
SilviaCue <152323429+SilviaCue@users.noreply.github.com>
2024-02-26 12:00:39 +0100

    ver introducir datos

--- TiendaOnline/GestionPedidos/models.py
+++ TiendaOnline/GestionPedidos/models.py
@@ -15,6 +15,9 @@ class Articulos(models.Model):
     seccion = models.CharField(max_length=80)
     precio = models.IntegerField()
 
+    def __str__(self):
+        return " El nombre es {} de la sección {} y su precio {} ".format(self.nombre, self.seccion, self.precio)
+
 
 class Pedido(models.Model):
     numero = models.IntegerField()
